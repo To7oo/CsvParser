a86db22796685621ceb185767bb583353c398ec9
Rodrigo Alonso <rodrigoalonso@roagro.com.ar>
2024-05-08 20:45:48 -0300

    🎨 Replace for double quotes

--- src/main.py
+++ src/main.py
@@ -8,43 +8,43 @@ print("""
 
 """)
 
-base_path = './data/'
+base_path = "./data/"
 list_of_files = os.listdir(base_path)
 
 files_count = len(list_of_files)
 
-print(f'Files found: {files_count}\n')
+print(f"Files found: {files_count}\n")
 
 valid_files = []
 invalid_files = []
 
 for file in list_of_files:
     
-    is_valid_file = os.path.isfile(base_path + file) & file.endswith('.csv')
-    print(f'Reading {base_path + file}')
+    is_valid_file = os.path.isfile(base_path + file) & file.endswith(".csv")
+    print(f"Reading {base_path + file}")
     
     if is_valid_file:
         valid_files.append(base_path + file)
     else:
         invalid_files.append(base_path + file)
 
-print('')
+print("")
 
 valid_files_count = len(valid_files)
 invalid_files_count = len(invalid_files)
 
-print(f'Valid files: {valid_files_count}')
-print(f'Invalid files: {invalid_files_count}\n')
+print(f"Valid files: {valid_files_count}")
+print(f"Invalid files: {invalid_files_count}\n")
 
 for invalid_file in invalid_files:
-    print(f'Invalid file: {invalid_file}')
+    print(f"Invalid file: {invalid_file}")
 
-print('')   
+print("")   
 
 newColumns = {
-    'Dirección de correo electrónico': 'email',
-    '¿Cuál es el nombre de tu emprendimiento?': 'name',
-    '¿Cuáles son las redes sociales de tu emprendimiento? Adjunta el link.': 'socialMedia'
+    "Dirección de correo electrónico": "email",
+    "¿Cuál es el nombre de tu emprendimiento?": "name",
+    "¿Cuáles son las redes sociales de tu emprendimiento? Adjunta el link.": "socialMedia"
 }
 
 filtered_keys =list(newColumns.values())
@@ -66,7 +66,7 @@ for path in valid_files:
     list_data.append(subset)
         
 
-print('Generate output...')
+print("Generate output...")
 pd.concat(list_data).to_csv(f"{base_path}output.csv", index=False)
 
-print('Done!')
+print("Done!")
